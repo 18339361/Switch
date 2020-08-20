b2dfea09f2fc460954713c27e6031648d1e9f5d4
18339361 <pranchal2001@gmail.com>
2020-08-20 18:07:57 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=0;
+        switch(a){
+            case 1:
+                System.out.println("A is one");
+                break;
+            case 2:
+                System.out.println("A is two");
+                break;
+            default:
+                System.out.println("A is neither 1 nor 2");
+        }
+    }
+
+    public static double CalculatorSwitch(double num1, double num2, String operator){
+        return 2.00;
+    }
+}
